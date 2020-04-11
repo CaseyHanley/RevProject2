6e3a4cb071de139d6187da887a0d5d75337ac64d
Joel Thomas <joel.tommy11@gmail.com>
2020-04-11 10:52:34 -0400

    JoelFirstPush

--- Project2FrontEndNG/src/app/component/joel/joel.component.ts
+++ Project2FrontEndNG/src/app/component/joel/joel.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JoelComponent implements OnInit {
 
+  cardPaid :string = ''; //change to "Payment Authorized" after they hit submit
+  address :string = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  submitCC() {
+    this.cardPaid = "Payment Authorized";
+  }
+  /*submitCC() {
+    //console.log(this.validateInputField());
+
+    let creditcard = new creditcard(this.cc_num, this.exp_date, this.cardholder_name);
+    this.pokeService.addPokemon(pokemon).subscribe(
+      (response) => {
+        console.log(response);
+        this.pokemonList.push(response);//dynamically adds it to list on front end
+
+      },
+      (response) => {
+        console.log("Failed to add pokemon.");
+        console.log(response);
+      }
+    );
+  }*/
 
 }
