0ff12085807f1a51dd9cd3b9d1aaa3d6f6b443ee
starsmine <starsmine.mj@gmail.com>
2020-04-12 14:16:53 -0400

    not working yet

--- Project2FrontEndNG/src/app/component/matt/matt.component.ts
+++ Project2FrontEndNG/src/app/component/matt/matt.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Address } from 'src/app/component/matt/models/Address'
+import { AddressService } from 'src/app/component/matt/service/address.service'
 
 @Component({
   selector: 'app-matt',
@@ -7,9 +10,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MattComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myaddress: AddressService) { }
 
   ngOnInit(): void {
+    this.viewMyAddress()
+  }
+
+  uid: number; //Need User ID
+  addressList: Array<Address>;
+  number: number;
+  streetName: string;
+  line2 : string;
+  city: String;
+  state: String;
+  postal: number;
+  SpecInstr: String;
+  valid :boolean = false;
+
+  viewMyAddress() {
+    this.myaddress.getAddresses(this.uid).subscribe(
+      (response) => {
+        this.addressList = response;
+      },
+
+      (response) => {
+        console.log("failed to grab address");
+      }
+
+    );
+  }
+
+  addAddress(){
+
+    if (this.validateInputFields()) {
+      let address = new Address ( this.number,  this.streetName,  this.line2, this.city, this.state, this.postal, this.SpecInstr)
+      this.myaddress.addAddress(address).subscribe(
+        (response) => {
+          console.log(response);
+          this.addressList.push(response);
+        },
+        (response) => {
+          console.log("Failed to add Addres")
+          console.log(response);
+        }
+      );
+    }
+  }
+
+  validateInputFields(): boolean {
+
+    if (this.number == undefined ||
+      this.streetName== undefined ||
+      this.streetName == "" ||
+      this.city == undefined ||
+      this.city == "" ||
+      this.state == undefined ||
+      this.state == "" ||
+      this.postal == undefined) {
+        this.valid = false;
+      return false;
+    } else {
+      this.valid = true;
+      return true;
+    }
   }
 
 }
+
+
---
+++ Project2FrontEndNG/src/app/component/matt/service/address.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Address} from '../models/Address'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddressService {
+
+  constructor(private http: HttpClient) { }
+  private headers = new HttpHeaders({'Content-Type':'application/json'});
+  
+  getAddresses(uid : number): Observable<Address[]> {
+    //Needs Correct URL
+    return this.http.get<Address[]>('urlforaddress/uid');
+  }
+
+  addAddress(add: Address): Observable<Address>{
+    return this.http.post<Address>('urlforaddress/'), add, {headers: this.headers}); 
+  }
+
+ // deleteMyAddress()
+}
