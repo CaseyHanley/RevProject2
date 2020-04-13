fb8c03cae622cc02ac59eab680945555e9f4831c
Joel Thomas <joel.tommy11@gmail.com>
2020-04-12 21:31:50 -0400

    orderPageDraft1

--- Project2FrontEndNG/src/app/component/joel/joel.component.ts
+++ Project2FrontEndNG/src/app/component/joel/joel.component.ts
@@ -7,32 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JoelComponent implements OnInit {
 
-  cardPaid :string = ''; //change to "Payment Authorized" after they hit submit
-  address :string = '';
+  //name
+  starterName :string = "";
+  appetizerName :string = "";
+  soupName :string = "";
+  saladName :string = "";
+  maincourseName :string = "";
+  dessertName :string = "";
+  beverageName :string = "";
+
+  //description
+  starterDesc :string = "";
+  appetizerDesc :string = "";
+  soupDesc :string = "";
+  saladDesc :string = "";
+  maincourseDesc :string = "";
+  dessertDesc :string = "";
+  beverageDesc :string = "";
+
+  //quantity
+  starterQuantity :number = 0;
+  appetizerQuantity :number = 0;
+  soupQuantity :number = 0;
+  saladQuantity :number = 0;
+  maincourseQuantity :number = 0;
+  dessertQuantity :number = 0;
+  beverageQuantity :number = 0;
+
+  //price
+  starterPrice :number = 0;
+  appetizerPrice :number = 0;
+  soupPrice :number = 0;
+  saladPrice :number = 0;
+  maincoursePrice :number = 0;
+  dessertPrice :number = 0;
+  beveragePrice :number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  submitCC() {
-    this.cardPaid = "Payment Authorized";
-  }
-  /*submitCC() {
-    //console.log(this.validateInputField());
 
-    let creditcard = new creditcard(this.cc_num, this.exp_date, this.cardholder_name);
-    this.pokeService.addPokemon(pokemon).subscribe(
-      (response) => {
-        console.log(response);
-        this.pokemonList.push(response);//dynamically adds it to list on front end
-
-      },
-      (response) => {
-        console.log("Failed to add pokemon.");
-        console.log(response);
-      }
-    );
-  }*/
 
 }
