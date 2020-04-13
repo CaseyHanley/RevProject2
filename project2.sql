296d6b314cb156f0f135c870c1abb4662828236d
Jerome Stanley <61161350+jhstanley21@users.noreply.github.com>
2020-04-13 08:50:22 -0400

    started login

--- project2.sql
+++ project2.sql
@@ -1,4 +1,4 @@
-create table usersTable(
+create table users(
 u_id number(10) primary key,
 username varchar2(50) unique not null,
 password varchar2(50) not null,
@@ -7,7 +7,8 @@ lastname varchar2(50) not null,
 account_type varchar2(50)not null
 
 );
-
+select * from menu_items;
+select * from users;
 
 create table deliveryAddress(
 a_id number(10)primary key,
@@ -20,10 +21,10 @@ comments varchar2(50)
 
 );
 
-drop table menuItems;
-select * from menuItems;
+drop table menu_Items;
+select * from menu_Items;
 
-create table menuItems(
+create table menu_Items(
 stock_id number(10),
 description varchar2(50),
 productname varchar2(50),
@@ -62,7 +63,7 @@ pickup number(1)
 );
 
 
-create table orderArchive(
+create table order_Archive(
 o_id number(10) primary key,
 u_id number(10),
 productname1 varchar2(50),
@@ -72,6 +73,8 @@ productname4 varchar2(50),
 productname5 varchar2(50),
 productname6 varchar2(50),
 productname7 varchar2(50),
+extra1 varchar2(50),
+extra2 varchar2(50),
 price number(10),
 comments varchar2(50),
 status varchar2(50),
@@ -88,12 +91,11 @@ discountamount number(10)
 );
 
 
-Alter Table orders add CONSTRAINT uid2_key FOREIGN KEY (u_id) REFERENCES userTable(u_id) ON DELETE CASCADE;
-Alter Table orders add CONSTRAINT aid_key FOREIGN KEY (a_id) REFERENCES deliveryAddress(a_id) ON DELETE CASCADE;
+Alter Table orders add CONSTRAINT uid2_key FOREIGN KEY (u_id) REFERENCES users(u_id) ON DELETE CASCADE;
 
-Alter Table orderArchive add CONSTRAINT uid3_key FOREIGN KEY (u_id) REFERENCES userTable(u_id) ON DELETE CASCADE;
-Alter Table orderArchive add CONSTRAINT aid2_key FOREIGN KEY (a_id) REFERENCES deliveryAddress(a_id) ON DELETE CASCADE;
-Alter Table orderArchive add CONSTRAINT oid_key FOREIGN KEY (o_id) REFERENCES orders(o_id) ON DELETE CASCADE;
+Alter Table order_Archive add CONSTRAINT uid3_key FOREIGN KEY (u_id) REFERENCES users(u_id) ON DELETE CASCADE;
+Alter Table order_Archive add CONSTRAINT oid_key FOREIGN KEY (o_id) REFERENCES orders(o_id) ON DELETE CASCADE;
 
-Alter Table deliveryAddress add CONSTRAINT uid_key FOREIGN KEY (u_id) REFERENCES userTable(u_id) ON DELETE CASCADE;
+Alter Table deliveryAddress add CONSTRAINT uid_key FOREIGN KEY (u_id) REFERENCES users(u_id) ON DELETE CASCADE;
+
 
