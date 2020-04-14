0d9058e3a19d80a5777239414b9aa6b7e1eab288
Jerome Stanley <61161350+jhstanley21@users.noreply.github.com>
2020-04-14 10:45:34 -0400

    lastest back end updates

--- Project2BackEnd/src/main/java/dev/stanley/controllers/MenuItemsController.java
+++ Project2BackEnd/src/main/java/dev/stanley/controllers/MenuItemsController.java
@@ -3,8 +3,9 @@ package dev.stanley.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import dev.stanley.beans.MenuItems;
 import dev.stanley.services.MenuItemsService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
 public class MenuItemsController {
 
 	@Autowired
