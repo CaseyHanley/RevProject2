03ddf13427f5acd440ebec9d7d3bd0b6806ac765
Jerome Stanley <61161350+jhstanley21@users.noreply.github.com>
2020-04-15 16:36:50 -0400

    front end update

--- Project2FrontEndNG/src/app/service/order.service.ts
+++ Project2FrontEndNG/src/app/service/order.service.ts
@@ -9,7 +9,6 @@ export class OrderService {
 
   public order: Order;
   constructor() { 
-    this.order = new Order(0, 0, 0, 0, '','',0, null, null, null, null, null, null, null);
-    this.order.menuItem1 = new MenuItem(1, "a lobster", "Lobster", 75);
+   
   }
 }
