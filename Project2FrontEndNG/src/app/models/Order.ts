33a0f1427bee3e1b390330fd59e0194bd5efe87e
ballsbs2 <ballsbs2@gmail.com>
2020-04-15 18:09:05 -0400

    started work to update order model

--- Project2FrontEndNG/src/app/models/Order.ts
+++ Project2FrontEndNG/src/app/models/Order.ts
@@ -1,4 +1,4 @@
-import { MenuItem } from './MenuItem';
+
 
 export class Order{
     o_id: number;
@@ -8,15 +8,15 @@ export class Order{
     comments: String;
     status: String;
     pickup: number;
-    menuItem1: MenuItem;
-    menuItem2: MenuItem;
-    menuItem3: MenuItem;
-    menuItem4: MenuItem;
-    menuItem5: MenuItem;
-    menuItem6: MenuItem;
-    menuItem7: MenuItem;
+    menuItem1: String;
+    menuItem2: String;
+    menuItem3: String;
+    menuItem4: String;
+    menuItem5: String;
+    menuItem6: String;
+    menuItem7: String;
 
-    constructor(o_id: number, u_id: number, a_id: number, price: number, comments: String, status: String, pickup: number, MenuItem1: MenuItem, MenuItem2: MenuItem, MenuItem3: MenuItem, MenuItem4: MenuItem, MenuItem5: MenuItem, MenuItem6: MenuItem, MenuItem7: MenuItem,){
+    constructor(o_id: number, u_id: number, a_id: number, price: number, comments: String, status: String, pickup: number, String1: String, String2: String, String3: String, String4: String, String5: String, String6: String, String7: String,){
         this.o_id = o_id;
         this.u_id = u_id;
         this.a_id = a_id;
@@ -24,12 +24,12 @@ export class Order{
         this.comments = comments;
         this.status = status;
         this.pickup = pickup;
-        this.menuItem1 = MenuItem1;
-        this.menuItem2 = MenuItem3;
-        this.menuItem3 = MenuItem2;
-        this.menuItem4 = MenuItem4;
-        this.menuItem5 = MenuItem5;
-        this.menuItem6 = MenuItem6;
-        this.menuItem7 = MenuItem7;
+        this.menuItem1 = String1;
+        this.menuItem2 = String3;
+        this.menuItem3 = String2;
+        this.menuItem4 = String4;
+        this.menuItem5 = String5;
+        this.menuItem6 = String6;
+        this.menuItem7 = String7;
     }
 }
