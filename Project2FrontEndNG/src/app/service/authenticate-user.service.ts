614b5b588f5d1fd8c1b65c4516dabcbf62ec6f29
Jerome Stanley <61161350+jhstanley21@users.noreply.github.com>
2020-04-15 16:54:36 -0400

    New front

--- Project2FrontEndNG/src/app/service/authenticate-user.service.ts
+++ Project2FrontEndNG/src/app/service/authenticate-user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { LoginService } from 'src/app/service/login.service';
-import { LoginUser } from 'src/app/models/loginuser';
+
 
 
 @Injectable({
@@ -12,42 +12,18 @@ export class AuthenticateUserService {
 
   username :string;
   password :string;
-  users :LoginUser;
+
 
   authenticate(username, password) {
     if(username !==null && password !==null){
-      // if (this.checkUser()){
-      //   sessionStorage.setItem('username', username)
-      // }
-      //Chef
-    if (username === "rome" && password === "pass") {
-      sessionStorage.setItem('username', username)
-      return true;
-    } 
-     //Customer
-    if (username === "casey" && password === "pass") {
-      sessionStorage.setItem('username', username)
-      return true;
-    } 
-    //Driver
-    if (username === "joel" && password === "pass") {
-      sessionStorage.setItem('username', username)
-      return true;
-    } 
-    //Manager
-    if (username === "sam" && password === "pass") {
-      sessionStorage.setItem('username', username)
-      return true;
-    } 
-    //Host
-    if (username === "matt" && password === "pass") {
-      sessionStorage.setItem('username', username)
-      return true;
-    } 
+    sessionStorage.setItem('username', username)
+    return true;
   }else {
       return false;
     }
 }
+
+
     
 
   isUserLoggedIn() :boolean {
@@ -64,19 +40,5 @@ export class AuthenticateUserService {
     
   }
 
-  checkUser() :boolean{
-    
-    this.userLogin.authUserByUP().subscribe(
-      (response) =>{
-        console.log(response)
-        return true;
-      },
-      (response) => {
-        console.log(response)
-        
-      }
-    )
-  return false;
-  }
 }
 
