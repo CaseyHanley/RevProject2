3cd2ea5a45ea7902d9c5722813c93eeeaaf24ba1
ballsbs2 <ballsbs2@gmail.com>
2020-04-16 16:44:10 -0400

    added the ability to add/delete items from the menu

--- Project2FrontEndNG/src/app/app.module.ts
+++ Project2FrontEndNG/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { StaffpageComponent } from './component/staffpage/staffpage.component';
 import { ContactusComponent } from './component/contactus/contactus.component';
 import { LoginService } from './service/login.service';
 import { OrderService } from './service/order.service';
+import { EditMenuComponent } from './edit-menu/edit-menu.component';
 
 
 @NgModule({
@@ -41,7 +42,8 @@ import { OrderService } from './service/order.service';
     BottomnavComponent,
     NewUserComponent,
     StaffpageComponent,
-    ContactusComponent
+    ContactusComponent,
+    EditMenuComponent
   
   ],
   imports: [
