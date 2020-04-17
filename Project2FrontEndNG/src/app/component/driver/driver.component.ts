fbe681fb1aa76e242bb66fc4b0e34526a6d333da
Jerome Stanley <61161350+jhstanley21@users.noreply.github.com>
2020-04-17 17:00:41 -0400

    commit

--- Project2FrontEndNG/src/app/component/driver/driver.component.ts
+++ Project2FrontEndNG/src/app/component/driver/driver.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DriverService } from 'src/app/service/driver.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-driver',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DriverComponent implements OnInit {
 
-  constructor() { }
+  constructor(private driver : DriverService, private router:Router) { }
 
   ngOnInit(): void {
   }
+  
+  driverUsername: string;
+  ousername :string;
+  oid :number;
 
 }
--- Project2FrontEndNG/src/app/service/order.service.ts
+++ Project2FrontEndNG/src/app/service/order.service.ts
@@ -6,6 +6,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -19,24 +20,23 @@ export class OrderService {
   private url = 'http://localhost:8080';
 
   constructor(private http: HttpClient) { }
- 
 
   getOrders(): Observable<Order>{
-    console.log(this.url + '/orders/' + sessionStorage.getItem("username"));
-    return this.http.get<Order>(this.url + '/orders/' + sessionStorage.getItem("username"));
+    console.log(this.url + '/order/' + sessionStorage.getItem("username"));
+    return this.http.get<Order>(this.url + '/order/' + sessionStorage.getItem("username"));
   }
 
   deleteOrders(id:number): Observable<boolean>{
-    console.log(this.url + '/orders/' + id);
-    return this.http.delete<boolean>(this.url + '/orders/' + id, {headers : this.authorized});
+    console.log(this.url + '/order/' + id);
+    return this.http.delete<boolean>(this.url + '/order/' + id, {headers : this.authorized});
   }
 
   addOrders(add: Order): Observable<Order>{
-    return this.http.post<Order>(this.url + '/orders', add ,{headers : this.headers});
+    return this.http.post<Order>(this.url + '/order/', add ,{headers : this.headers});
   } 
 
   updateOrders(update: Order): Observable<Order>{
-    return this.http.put<Order>(this.url + '/orders', JSON.stringify(update));
+    return this.http.put<Order>(this.url + '/order', update, {headers : this.headers});
   } 
 
 
