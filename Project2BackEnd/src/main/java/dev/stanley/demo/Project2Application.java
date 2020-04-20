3948674e86f256ae9430e0ff0552ece7defc38af
Casey.T.Hanley <Caseythomashanley@hotmail.com>
2020-04-20 01:07:57 -0400

    Cors Test

--- Project2BackEnd/src/main/java/dev/stanley/demo/Project2Application.java
+++ Project2BackEnd/src/main/java/dev/stanley/demo/Project2Application.java
@@ -3,8 +3,11 @@ package dev.stanley.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @ComponentScan("dev.stanley")
@@ -16,4 +19,18 @@ public class Project2Application {
 		SpringApplication.run(Project2Application.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurerAdapter corsConfigurer() {
+		
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("http://devhanley-bucket.s3-website.us-east-2.amazonaws.com");
+			}
+		};
+	}
+	
+	
+	
+	
 }
