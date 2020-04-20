a25cb8884984d653427d68e107b2d11867ee991f
Casey.T.Hanley <Caseythomashanley@hotmail.com>
2020-04-20 04:35:07 -0400

    git test 9

--- Project2BackEnd/src/main/java/dev/stanley/demo/Project2Application.java
+++ Project2BackEnd/src/main/java/dev/stanley/demo/Project2Application.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
@@ -20,16 +22,20 @@ public class Project2Application {
 		SpringApplication.run(Project2Application.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurerAdapter corsConfigurer() {
-		
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*").allowedOrigins("http://devhanley-bucket.s3-website.us-east-2.amazonaws.com");
-			}
-		};
+	@Configuration
+	public class MyConfiguration {
+
+	    @Bean
+	    public WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurerAdapter() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/**");
+	            }
+	        };
+	    }
 	}
+
 	
 	
 	
